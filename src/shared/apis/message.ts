231b0298955c4b8c915c770f1a1e0288e1d07c37
quangkhoa <quangkhoatb@gmail.com>
2024-01-31 12:35:37 +0700

    kk

--- src/shared/apis/message.ts
+++ src/shared/apis/message.ts
@@ -2,7 +2,7 @@ import api from '../client';
 
 const messageApi = {
   addMessage: (tableId: string, params: AddMessageReq[]) =>
-    api.post(`/v1/rows/${tableId}`, params, { baseURL: 'https://api.tablebackend.com' }),
+    api.post(`/v1/rows/${tableId}`, params, { baseURL: 'https://v1.appbackend.io' }),
 };
 
 export default messageApi;
