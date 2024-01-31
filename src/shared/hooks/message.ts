f30b0e70126d5c632bdd2b0d4ca09822833eac8f
quangkhoa <quangkhoatb@gmail.com>
2024-01-31 12:15:48 +0700

    link appbackend

--- src/shared/hooks/message.ts
+++ src/shared/hooks/message.ts
@@ -2,5 +2,5 @@ import useSWR from "swr"
 import { fetcher } from ".."
 
 export const useMessages = (tableId: string) => {
-  return useSWR(`v1/rows/${tableId}`, (url) => fetcher(url, { baseURL: 'https://api.appbackend.io' }))
+  return useSWR(`v1/rows/${tableId}`, (url) => fetcher(url, { baseURL: 'https://v1.appbackend.io' }))
 }
